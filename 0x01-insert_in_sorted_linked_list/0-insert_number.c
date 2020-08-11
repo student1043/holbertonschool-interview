a7b34d67cb9c43ec5dd21562687d6c8b0800fa82
student1043 <1043@holbertonschool.com>
2020-08-11 16:07:43 +0100

    First Push

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * *insert_node - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * @number: the number to be added
+ * Return: number of nodes
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
