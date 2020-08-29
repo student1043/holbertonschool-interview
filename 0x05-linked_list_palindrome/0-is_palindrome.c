1dd65222c99d2d7b124cd2388090b142d5f009c6
student1043 <1043@holbertonschool.com>
2020-08-29 18:14:04 +0100

    Task 0

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check the code for Holberton School students.
+ * @head: the head of the linked list
+ * Return: Always 0.
+ */
+int is_palindrome(listint_t **head)
+{
+if(head == NULL) {
+    return (1);
+}
+listint_t *mainnode;
+listint_t *reversedlist;
+mainnode = *head;
+reversedlist = *head;
+reversedlist = reverse(reversedlist);
+while (mainnode != NULL)
+{
+if(mainnode->n != reversedlist->n)
+{
+return(0);
+}
+mainnode = mainnode->next;
+reversedlist = reversedlist->next;
+}
+return(1);
+}
+
+
+/**
+ * reverse - check the code for Holberton School students.
+ * @head: the head of the linked list
+ * Return: Always 0.
+ */
+listint_t *reverse(listint_t *head)
+{
+listint_t *prev = NULL;
+listint_t *current = head; 
+listint_t *next = NULL;
+while(current != NULL)
+{
+next = current->next; 
+current->next = prev; 
+prev = current; 
+current = next; 
+} 
+return(prev); 
+} 
