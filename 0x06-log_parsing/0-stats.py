85c318ee73fc024428d8bf587e224756f56556b6
student1043 <1043@holbertonschool.com>
2020-09-02 21:29:07 +0100

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -8,18 +8,25 @@ i = 0
 l = 0
 k = 0
 sumof = 0
-for i in range(len(data)):
-    for line in data:
-        sumof += int(line.split('1.1"')[1].split(" ")[2])
-        statuscodes.append(int(line.split('1.1"')[1].split(" ")[1]))
-        k += 1
-    try:
-        if k == 10:
-            print("File size:", sumof)
-            for l in range(len(presets)):
-                if presets[l] in statuscodes:
-                    print("{}: {}".format(presets[l], statuscodes.count(presets[l])))
-            k = 0
-            statuscodes.clear()
-    except (KeyboardInterrupt, SystemExit):
-        pass
+try:
+    for i in range(len(data)):
+        for line in data:
+            sumof += int(line.split('1.1"')[1].split(" ")[2])
+            statuscodes.append(int(line.split('1.1"')[1].split(" ")[1]))
+            k += 1
+            if k == 10:
+                print("File size:", sumof)
+                for l in range(len(presets)):
+                    if presets[l] in statuscodes:
+                        print("{}: {}".format(presets[l], statuscodes.count(presets[l])))
+                k = 0
+                statuscodes.clear()
+except Exception:
+    pass
+finally:
+    print("File size:", sumof)
+    for l in range(len(presets)):
+        if presets[l] in statuscodes:
+            print("{}: {}".format(presets[l], statuscodes.count(presets[l])))
+    k = 0
+    statuscodes.clear()
