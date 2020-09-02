5faecc143154e6a8db22dca281f5d92daf0310c6
student1043 <1043@holbertonschool.com>
2020-09-02 22:17:48 +0100

    Fixing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,26 +6,26 @@ status_presets = ["200", "301", "400", "401", "403", "404", "405", "500"]
 statuscodes = [0, 0, 0, 0, 0, 0, 0, 0]
 i = 0
 k = 0
-l = 0
+c = 0
 try:
     for line in sys.stdin:
         splitline = line.split()
         if len(splitline) > 2:
             if splitline[-2] in status_presets:
-                l = status_presets.index(splitline[-2])
-                statuscodes[l] = statuscodes[l] + 1
+                c = status_presets.index(splitline[-2])
+                statuscodes[c] = statuscodes[c] + 1
             i += 1
             k = k + int(splitline[-1])
         if i == 10:
             i = 0
             print("File size: {}".format(k))
-            for l in range(8):
-                if statuscodes[l] != 0:
-                    print('{}: {}'.format(status_presets[l], statuscodes[l]))
+            for c in range(8):
+                if statuscodes[c] != 0:
+                    print('{}: {}'.format(status_presets[c], statuscodes[c]))
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(k))
-    for l in range(8):
-        if statuscodes[l] != 0:
-            print("{}: {}".format(status_presets[l], statuscodes[l]))
+    for c in range(8):
+        if statuscodes[c] != 0:
+            print("{}: {}".format(status_presets[c], statuscodes[c]))
