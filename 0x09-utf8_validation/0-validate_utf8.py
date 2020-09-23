2367ec03b29e608742cef18a6e26486385a8b3c1
student1043 <1043@holbertonschool.com>
2020-09-23 23:00:34 +0100

    Fix

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,18 +6,30 @@ def validUTF8(data):
     """
     ValidUTF8 Function
     """
-    count = 0
-    for byte in data:
-        if count:
+    k = 0
+    while k < len(data):
+        lead = data[k]
+        if 0 <= lead < 128:
+            k += 1
+        elif 128 <= lead < 192:
             return False
-        if byte < 128:
-            continue
-        elif byte < 224:
-            count = 1
-        elif byte < 240:
-            count = 2
-        elif byte < 248:
-            count = 3
+        elif 192 <= lead < 224:
+            if k+1 < len(data) and 128 <= data[k+1] < 192:
+                k += 2
+            else:
+                return False
+        elif 224 <= lead < 240:
+            if k+2 < len(data) and 128 <= data[k+1] < 192 and 128 <= data[k+2] < 192:
+                k += 3
+            else:
+                return False
+        elif 240 <= lead < 248:
+            if k+3 < len(data) and 128 <= data[k+1] < 192 and 128 <= data[k+2] < 192 and 128 <= data[k+3] < 192:
+                k += 4
+            else:
+                return False
+        elif 248 <= lead <= 255:
+            return False
         else:
-            return True          
+            return True
     return True
