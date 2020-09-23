d82c82eaa0c465220359d1d8c736d555e3a1408d
student1043 <1043@holbertonschool.com>
2020-09-23 22:56:56 +0100

    Fixing

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,22 +8,16 @@ def validUTF8(data):
     """
     count = 0
     for byte in data:
-        if byte >= 128 and byte <= 191:
-            if not count:
-                return False
-            count -= 1
+        if count:
+            return False
+        if byte < 128:
+            continue
+        elif byte < 224:
+            count = 1
+        elif byte < 240:
+            count = 2
+        elif byte < 248:
+            count = 3
         else:
-            if count:
-                return False
-            if byte < 128:
-                continue
-            elif byte < 224:
-                count = 1
-            elif byte < 240:
-                count = 2
-            elif byte < 248:
-                count = 3
-            else:
-                return False
-                
-    return count == 0
+            return True          
+    return True
