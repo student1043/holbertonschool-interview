7ce4b457e4ef5669bc3adcc1d6aed73e09639a0f
student1043 <1043@holbertonschool.com>
2020-10-16 13:15:33 +0100

    Method 2

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,8 +5,11 @@ Minimum Operations
 
 def minOperations(n):
     """Min OP"""
-    if n <= 1:
-        return 0
-    for counter in range(2, n + 1):
-        if n % 1 == 0:
-            return minOperations(n // counter) + counter
+    counter = 2
+    operations = 0
+    while n > 1:
+        while n % counter == 0:
+            operations += counter
+            n = n // counter
+        counter += 1
+    return operations
