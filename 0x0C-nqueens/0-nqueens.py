740167c7869e2c585772b0e6c08ed44ec6eee7b6
student1043 <1043@holbertonschool.com>
2020-10-23 01:25:36 +0100

    Finished

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" N Queens """
+import sys
+
+
+def isValid(chess, row, pos):
+    """ Validating That The Queen Placement is Correct """
+    for i in range(row):
+        if(chess[i] == pos):
+            return False
+        if(chess[i] + row - i == pos):
+            return False
+        if(chess[i] + i - row == pos):
+            return False
+    return True
+
+
+def SetChess(chess, row):
+    """ Setting the Chessboard Up """
+    for pos in range(len(chess)):
+        if isValid(chess, row, pos):
+            chess[row] = pos
+            if row < len(chess) - 1:
+                SetChess(chess, row + 1)
+            else:
+                print([[pos, chess[pos]] for pos in range(len(chess))])
+
+
+if len(sys.argv) != 2:
+    print("nqueens N")
+    exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+chess = [-1 for i in range(int(sys.argv[1]))]
+SetChess(chess, 0)
---
+++ 0x0C-nqueens/README.md
@@ -0,0 +1,1 @@
+N Queens Backtracking Algorithm in Python
