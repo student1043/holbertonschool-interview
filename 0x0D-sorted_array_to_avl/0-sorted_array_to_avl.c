c2822d0a6d00f94e86ca26a45b0bb45f477a572f
student1043 <1043@holbertonschool.com>
2020-11-04 23:43:45 +0100

    Update 0-sorted_array_to_avl.c

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include "binary_trees.h"
 
 /**
@@ -8,19 +10,16 @@
  * @size: size of the array
  * Return: 0 on success, error code on failure
  */
-avl_t *sorted_array_to_avl(int *array, size_t size)
-{
-        int count;
-        if (!array || size < 1)
-                return (NULL);
-        for (count = 1; count < (int)size; count++)
-        {
-                if (array[count] < array[count - 1])
-                    return (NULL);
-        }
-        return (Method(array, 0, size - 1, NULL));
+avl_t * sorted_array_to_avl(int * array, size_t size) {
+    int count;
+    if (!array || size < 1)
+        return (NULL);
+    for (count = 1; count < (int) size; count++) {
+        if (array[count] < array[count - 1])
+            return (NULL);
+    }
+    return (Method(array, 0, size - 1, NULL));
 }
-
 
 /**
  * Method - Entry point
@@ -30,22 +29,19 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
  * @parent: AVL Parent
  * Return: 0 on success, error code on failure
  */
-avl_t *Method(int *array, int start, int end, avl_t *parent)
-{
-        if (start > end)
-        {
-                return (NULL);
-        }
-        int mid = (start + end) / 2;
-        avl_t *bst;
-        bst = malloc(sizeof(avl_t));
-        if (!bst)
-        {
-                return (NULL);
-        }
-        bst->n = array[mid];
-        bst->parent = parent;
-        bst->left = Method(array, start, mid - 1, bst);
-        bst->right = Method(array, mid + 1, end, bst);
-        return (bst);
+avl_t * Method(int * array, int start, int end, avl_t * parent) {
+    if (start > end) {
+        return (NULL);
+    }
+    int mid = (start + end) / 2;
+    avl_t * bst;
+    bst = malloc(sizeof(avl_t));
+    if (!bst) {
+        return (NULL);
+    }
+    bst -> n = array[mid];
+    bst -> parent = parent;
+    bst -> left = Method(array, start, mid - 1, bst);
+    bst -> right = Method(array, mid + 1, end, bst);
+    return (bst);
 }
