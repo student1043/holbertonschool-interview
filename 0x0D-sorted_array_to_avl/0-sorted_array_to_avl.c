552a04c281fca2cb6740b49f3338ab793f666391
student1043 <1043@holbertonschool.com>
2020-11-04 23:28:04 +0100

    First Try

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,22 +10,27 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-
+if (!array)
+{
+return (NULL);
+}
+return (Method(array, 0, size - 1, NULL));
 }
 
 
 /**
  * Method - Entry point
  * @array: the array
- * @size: size of the array
+ * @start: start of the array
+ * @end: end of array
  * @parent: AVL Parent
  * Return: 0 on success, error code on failure
  */
 avl_t *Method(int *array, int start, int end, avl_t *parent)
 {
-if(start > end)
+if (start > end)
 {
-return(NULL);
+return (NULL);
 }
 int mid = (start + end) / 2;
 avl_t *bst;
@@ -36,7 +41,7 @@ return (NULL);
 }
 bst->n = array[mid];
 bst->parent = parent;
-bst->left = Method(array, start, mid-1, bst);
+bst->left = Method(array, start, mid - 1, bst);
 bst->right = Method(array, mid + 1, end, bst);
-return(bst);
+return (bst);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -11,10 +11,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -22,6 +22,9 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
 avl_t *sorted_array_to_avl(int *array, size_t size);
+
 avl_t *Method(int *array, int start, int end, avl_t *parent);
+
+void binary_tree_print(const binary_tree_t *);
 
 #endif
