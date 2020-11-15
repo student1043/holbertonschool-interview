5c5657d9f7216718d630974c781141476d62e7f8
student1043 <1043@holbertonschool.com>
2020-11-15 18:30:36 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,12 +5,15 @@ request(url, function (err, response, body) {
   if (err) throw err;
   const json = JSON.parse(body);
   let i = 0;
+  let chars = []
   for (i = 0; i < json.characters.length; i++) {
     const url1 = json.characters[i];
     request(url1, function (err, response, data) {
       if (err) throw err;
-      const json1 = JSON.parse(data);
-      console.log(json1.name);
+      let json1 = JSON.parse(data);
+      chars.push(JSON.stringify(json1.name));
+
     });
   }
+  console.log(chars);
 });
