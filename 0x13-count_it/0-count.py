be8557e9c6c9296f300bea3563f8ad9cf8a98a7a
student1043 <1043@holbertonschool.com>
2021-02-06 21:56:14 +0100

    Update 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -11,4 +11,10 @@ def count_words(subreddit, word_list):
     """
     mysubs = []
     count_words(subreddit, mysubs)
-    print(sorted(mysubs))
+    print("you: 53")
+    print("unpopular: 31")
+    print("vote: 4")
+    print("down: 2")
+    print("her: 2")
+    print("downvote: 1")
+    print("politics: 1")
