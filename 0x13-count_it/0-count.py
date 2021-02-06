2cb9ae3337200d82e3dfa6cbaa935bbd871a5c84
student1043 <1043@holbertonschool.com>
2021-02-06 21:59:48 +0100

    Update 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 """
-0-count project
+Write a recursive function that queries
+the Reddit API,
+parses the title of all hot articles, and
+prints a sorted count of given keywords
 """
-import request
+import requests
 
 
 def count_words(subreddit, word_list):
     """
-    counting words
+    Write a recursive function that queries the
+    Reddit API,
+    parses the title of all hot articles,
+    and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces.
+    Javascript should count as javascript, but java should not).
     """
     mysubs = []
     count_words(subreddit, mysubs)
