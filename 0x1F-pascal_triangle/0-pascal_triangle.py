df94f434bf5baab1a6bc0760dd3b778ebed713bd
student1043 <1043@holbertonschool.com>
2021-08-04 20:04:40 +0200

    Update

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -5,15 +5,22 @@
 
 
 def pascal_triangle(n):
-    pascal = []
+    Pascal = []
     if n <= 0:
-        return pascal
-    tree = [[1]]
-    for x in range(1, n):
-        pascal = [1]
-        for y in range(1, x):
-            pascal.append(tree[x-1][y-1] + tree[x-1][y])
-        pascal.append(1)
-        tree.append(pascal)
-        pascal = []
-    return tree
+        return Pascal
+
+    temp_list = []
+    for i in range(1, n + 1):
+        for j in range(i):
+            if(j == 0):
+                temp_list.append(1)
+                continue
+            elif(j == i - 1):
+                temp_list.append(1)
+                continue
+            else:
+                temp_list.append(Pascal[i-2][j]+Pascal[i-2][j-1])
+        Pascal.append(temp_list)
+        temp_list = []
+
+    return Pascal
