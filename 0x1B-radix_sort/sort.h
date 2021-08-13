5659b2521aaf7df7c3183dffc2139629e24e1f3c
student1043 <1043@holbertonschool.com>
2021-08-13 20:06:31 +0200

    Fix

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -7,7 +7,7 @@
 void radix_sort(int *array, size_t size);
 
 int get_max(int *array, size_t size);
-int current_digit_sort(int *array, ssize_t size, int *tmp, long exp);
 
 void print_array(const int *array, size_t size);
 #endif
+
