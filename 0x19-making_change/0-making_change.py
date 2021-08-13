95a1bf2ab0ac78cb2971d936df92e4baee24aad5
student1043 <1043@holbertonschool.com>
2021-08-13 12:39:00 +0200

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,15 +3,14 @@
 Making Change
 """
 
+
 def makeChange(coins, total):
     """ Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total
     """
     MAX = float("inf")
-
-    dp = [0] + [MAX for i in range (total)]
-
-    for i in range (1,total+1):
+    dp = [0] + [MAX for i in range(total)]
+    for i in range(1, total+1):
 
         smallest = MAX
 
