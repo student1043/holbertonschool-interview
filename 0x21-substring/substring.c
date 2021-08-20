b57edaece0cd7a1e976dd1c8e2906e1596ff8e62
student1043 <1043@holbertonschool.com>
2021-08-20 17:13:20 +0200

    Update substring.c

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -11,38 +11,38 @@
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int len = strlen(s);
-	int wordLen = strlen(words[0]), j, i = 0, k;
-	int *output = (int *)malloc(len * sizeof(int));
-	*n = 0;
-	int *found = (int *)malloc(nb_words * sizeof(int));
+int len = strlen(s);
+int wordLen = strlen(words[0]), j, i = 0, k;
+int *output = (int *)malloc(len * sizeof(int));
+*n = 0;
+int *found = (int *)malloc(nb_words * sizeof(int));
 
-	while (i <= len - nb_words * wordLen)
-	{
-	for (j = 0; j < nb_words; j++)
-	{
-		found[j] = 0;
-	}
-	for (j = 0; j < nb_words; j++)
-	{
-		for (k = 0; k < nb_words; k++)
-		{
-		if (found[k] == 0 && strncmp(s + i + j * wordLen, words[k], wordLen) == 0)
-		{
-			found[k] = 1;
-			break;
-		}
-		}
-		if (k == nb_words)
-		{
-		break;
-		}
-	}
-	if (j == nb_words)
-	{
-		output[(*n)++] = i;
-	}
-	i++;
-	}
-	return (output);
+while (i <= len - nb_words * wordLen)
+{
+for (j = 0; j < nb_words; j++)
+{
+found[j] = 0;
+}
+for (j = 0; j < nb_words; j++)
+{
+for (k = 0; k < nb_words; k++)
+{
+if (found[k] == 0 && strncmp(s + i + j * wordLen, words[k], wordLen) == 0)
+{
+found[k] = 1;
+break;
+}
+}
+if (k == nb_words)
+{
+break;
+}
+}
+if (j == nb_words)
+{
+output[(*n)++] = i;
+}
+i++;
+}
+return (output);
 }
