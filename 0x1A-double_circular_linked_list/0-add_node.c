7f4996e72ca3325c882fe79e8228d2be9d0882e9
student1043 <1043@holbertonschool.com>
2021-09-13 22:27:39 +0200

    add

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * add_node_end - Print informations about each element of a list
+ * @list: A list
+ * @str: string
+ * Return: void
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *Node, *first, *last;
+
+	if (!str)
+	{
+	return (NULL);
+	}
+	Node = malloc(sizeof(List));
+	if (!Node)
+	{
+	return (NULL);
+	}
+	Node->str = strdup(str);
+	if (!Node->str)
+	{
+	return (NULL);
+	}
+	if (!list || *list == NULL)
+	{
+	Node->next = Node;
+	Node->prev = Node;
+	*list = Node;
+	return (Node);
+	}
+	else
+	{
+	first = *list;
+	last = first->prev;
+	Node->next = first;
+	Node->prev = last;
+	last->next = Node;
+	first->prev = Node;
+
+	return (Node);
+	}
+}
+
+/**
+ * add_node_begin - Print informations about each element of a list
+ * @list: a list
+ * @str: string
+ * Return: void
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *Node, *first, *last;
+
+	if (!str)
+	{
+	return (NULL);
+	}
+	Node = malloc(sizeof(List));
+	if (!Node)
+	{
+	return (NULL);
+	}
+	Node->str = strdup(str);
+	if (!Node->str)
+	{
+	return (NULL);
+	}
+	if (!list || !*list)
+	{
+	Node->next = Node;
+	Node->prev = Node;
+	*list = Node;
+	return (Node);
+	}
+	first = *list;
+	last = first->prev;
+	Node->next = first;
+	Node->prev = last;
+	last->next = Node;
+	first->prev = Node;
+	*list = Node;
+
+	return (Node);
+}
