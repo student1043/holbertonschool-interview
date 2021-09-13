fd2d45ac560333d971719984419876f046340f43
student1043 <1043@holbertonschool.com>
2021-09-13 23:58:27 +0200

    add

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -8,26 +8,16 @@ def isWinner(x, nums):
     """
     iswinner
     """
-    if type(nums) != list and len(nums) != x:
-        return None
-    maxi = 10000
-    b = [1 for x in range(maxi + 1)]
-    b[0], b[1] = 0, 0
-    ind = 2
-    while ind <= 1000:
-        if b[ind] == 1:
-            y = ind + ind
-            while y <= maxi:
-                b[y] = 0
-                y += ind
-        ind += 1
-    c = [0 for x in range(maxi + 1)]
-    for x in range(2, maxi + 1):
-        c[x] = c[x - 1] + b[x]
-    if max(nums) == 1:
+    p = [0] * 11 ** 5
+    pr = [0] * 11 ** 5
+    k = 0
+    for i in range(2, 11 ** 5):
+        if p[i] == 0:
+            k += 1
+            for j in range(i, 11 ** 5, i):
+                p[j] = 1
+        pr[i] = k
+    if pr[max(nums)] % 2 == 0:
         return "Maria"
     else:
-        if c[max(nums)] % 2 == 1:
-            return "Ben"
-        else:
-            return "Maria"
+        return "Ben"
